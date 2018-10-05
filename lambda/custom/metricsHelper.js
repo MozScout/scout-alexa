05846f17e95a2794e9856e30cee9775231a9238b
tamarahills <thills@mozilla.com>
2018-10-05 15:25:25 -0700

    Make timestamp in secs (#139)
    * Add additional metrics for play ordinal, play subject, and open pocket events.
    
    * Timestamp should be in seconds, not milliseconds

--- lambda/custom/metricsHelper.js
+++ lambda/custom/metricsHelper.js
@@ -20,7 +20,7 @@ class metricsHelper {
         actionKeys = [
           {
             action: action,
-            time: Date.now(),
+            time: Date.now() / 1000,
             cxt_view: 'command',
             cxt_command: command,
             item_id: item_id ? item_id : undefined
@@ -30,7 +30,7 @@ class metricsHelper {
         actionKeys = [
           {
             action: action,
-            time: Date.now(),
+            time: Date.now() / 1000,
             cxt_view: 'command',
             item_id: item_id
           }
